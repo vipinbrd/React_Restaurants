e580c9966ad9a8cf69d4921cae90d0450a020697
Vipinbrd <vipinbarode7@gmail.com>
2025-03-25 16:40:24 +0530

    header added

---
+++ react_restaurants/src/components/Layout/Header.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { ShoppingCart } from "lucide-react"; 
+import banner from "../../assets/React_Header.png"
+
+export const Header = () => {
+  const [cartQuantity, setCartQuantity] = useState(0);
+
+  return (
+    <>
+    <header className="flex justify-between items-center p-4 bg-red-950 text-white shadow-md">
+  
+      <h1 className="text-2xl font-semibold">ReactMeals</h1>
+
+    
+      <button className="relative flex items-center gap-2 bg-blue-600 px-4 py-2 rounded-lg hover:bg-blue-700 transition">
+        <ShoppingCart size={24} />
+        <span className="text-lg font-medium">Cart</span>
+        
+        <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-2 py-1 rounded-full">
+          {cartQuantity}
+        </span>
+      </button>
+    </header>
+     <div>
+        <img src={banner}></img>
+     </div>
+    </>
+  );
+};
+
+
