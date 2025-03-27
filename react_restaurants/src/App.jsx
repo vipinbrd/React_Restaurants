0e2292f65bf759a3a43200678f9199ae888ccd44
Vipinbrd <vipinbarode7@gmail.com>
2025-03-27 16:45:20 +0530

    Dummy meals added

--- react_restaurants/src/App.jsx
+++ react_restaurants/src/App.jsx
@@ -4,14 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './components/Layout/Header'
 import { MealSummary } from './components/Meals/MealSummary'
+import { Meals } from './components/Meals/Meals'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div className="bg-[#4c4343]">
       <Header/>
+     
       <MealSummary/>
+      <Meals/>
+      </div>
     </>
   )
 }
---
+++ react_restaurants/src/components/Meals/Meals.jsx
@@ -0,0 +1,20 @@
+import data from "./DummyReciepies.json";
+
+export function Meals() {
+  return (
+    <div className="w-[70%] mx-auto bg-white border-2 rounded-lg shadow-md p-6">
+      <ul className="flex flex-col items-start space-y-3">
+        {data.map((ele) => (
+          <li
+            key={ele.name}
+            className="w-full border-b-2 border-indigo-400 py-3 px-4 hover:bg-gray-100 rounded-md transition-all"
+          >
+            <h2 className="text-xl font-bold text-gray-800">{ele.name}</h2>
+            <p className="text-gray-600">{ele.title}</p>
+            <p className="text-green-600 font-semibold">${ele.price.toFixed(2)}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
