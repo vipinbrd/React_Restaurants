6d56848a78ea181a6fb89061a497fc21c092e302
Vipinbrd <vipinbarode7@gmail.com>
2025-03-27 16:10:04 +0530

    Meal summary added

--- react_restaurants/src/App.jsx
+++ react_restaurants/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './components/Layout/Header'
+import { MealSummary } from './components/Meals/MealSummary'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Header/>
+      <MealSummary/>
     </>
   )
 }
---
+++ react_restaurants/src/components/Meals/MealSummary.jsx
@@ -0,0 +1,14 @@
+export function MealSummary() {
+    return (
+      <div className="w-[60%] mx-auto bg-gray-700 text-white p-6 rounded-lg shadow-lg text-center space-y-4">
+        <h1 className="text-2xl font-bold">Delicious Food, Delivered To You</h1>
+        <p className="text-lg">
+          Choose your favorite meal from our wide selection of available dishes and enjoy a delicious lunch or dinner at home.
+        </p>
+        <p className="text-lg">
+          All our meals are cooked with high-quality ingredients, prepared just in time, and of course, crafted by experienced chefs!
+        </p>
+      </div>
+    );
+  }
+  
